68438eb310f128a0a449e31cd8a6ace08b254c7a
armin <arminp61@gmail.com>
2022-09-05 16:41:50 +0430

    final version

--- BTC_alarm.py
+++ BTC_alarm.py
@@ -1,4 +1,6 @@
 
+import sys
+import keyboard
 import pyttsx3
 import cryptocompare
 import time
@@ -10,7 +12,7 @@ class BitcoinAlarmer:
         self.low_threshold = low_threshold
         self.high_threshold = high_threshold
         self.current_price = None
-        #self.engine = pyttsx3.init()
+        self.engine = None
         self.x = Thread(target=self.get_price)
         self.x.start()
 
@@ -27,31 +29,38 @@ class BitcoinAlarmer:
             #print(self.current_price)
             time.sleep(5)
 
+    
     def alarm(self):
         while True:
+            if keyboard.is_pressed("esc"):
+                print('you pressed esc')
+                sys.exit()
             if self.current_price > int(self.high_threshold):
-                playsound('/Githubprojects/bitcoin_alarmer/up.wav')
-
+                self.sound = '/Githubprojects/bitcoin_alarmer/up.wav'
+                playsound(self.sound)
                 self.engine = pyttsx3.init()
                 self.engine.say('Bitcoin price is higher then  %s'% self.high_threshold)
                 self.engine.runAndWait()
                 self.engine = None
+            elif keyboard.is_pressed("esc"):
+                print('you pressed esc')
+                sys.exit()
             elif self.current_price < int(self.low_threshold):
-                playsound('/Githubprojects/bitcoin_alarmer/up.wav')
+                playsound(self.sound)
                 self.engine = pyttsx3.init()
                 self.engine.say('Bitcoin price is lower then  %s'% self.low_threshold)
                 self.engine.runAndWait()
                 self.engine = None
             else:
-                print(self.current_price)
+                pass               
 
-            time.sleep(10)
+    
 
-high_threshold =  input('please enter the high threshold : ')
-low_threshold = input('please enter the low threshold : ')
+#high_threshold =  input('please enter the high threshold : ')
+#low_threshold = input('please enter the low threshold : ')
+#obj = BitcoinAlarmer(19800,19700)
 
-if __name__ == '__main__':
-    BitcoinAlarmer(low_threshold,high_threshold)
+
 
 
 
